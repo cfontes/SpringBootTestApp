31e3b9c0fec9f45d3b48cf74609d7d106988219f
cfontes <cristianofontes@gmail.com>
2015-02-14 15:05:39 -0200

    Changing package name and testing spring boot

--- src/main/java/br/com/omcrud/AppStart.java
+++ src/main/java/br/com/omcrud/AppStart.java
@@ -1,18 +1,18 @@
-package br.com.kanbanapp;
+package br.com.omcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author cfontes
- * @Date 14/02/2015.
- */
 @RestController
 @EnableAutoConfiguration
 public class AppStart {
 
+    /**
+     * Starts spring boot application
+     * @param args args from user
+     */
     public static void main(String[] args) {
         SpringApplication.run(AppStart.class, args);
     }
